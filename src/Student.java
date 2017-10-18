5c3d8a50b70d222ca5ecb682886bfd97e06930c5
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-18 10:42:54 -0500

    created student:

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private Integer id;
+    private Integer grade;
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public Integer getId() { return id; }
+
+    public void setId(Integer id) { this.id = id; }
+
+    public Integer getGrade() { return grade; }
+
+    public void setGrade(Integer grade) { this.grade = grade; }
+}
