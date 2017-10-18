2d7be3b1f1f5fcbbfe44465be6be8b6ee906e80f
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-18 10:51:44 -0500

    Save

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Student> studentList = new ArrayList();
+        ArrayList<Student> studentList = new ArrayList<>();
         for (int i = 0; i < 15; ++i) {
             studentList.add(new Student("Tyler", "Jurecki", i, 3));
         }
